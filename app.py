56943cfec1f8c416a997df61babbc8498c626f16
Ashwin Kinikar <ash@Ashwins-MacBook-Pro.local>
2023-10-25 16:54:35 -0700

    added code

--- app.py
+++ app.py
@@ -132,4 +132,5 @@ def get_stock_info():
         return jsonify({'error': 'Failed to fetch data from OpenAI GPT-3 or yfinance.'}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
